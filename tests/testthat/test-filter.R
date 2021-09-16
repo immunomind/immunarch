ab216305d6ecbf1b2bedd03e42c0af81f33a6727
Aleksandr Popov <fire_2005@mail.ru>
2021-09-16 11:59:10 +0300

    fixes: calculated expected samples for default test data; small fixes

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -5,47 +5,46 @@ lessthan <- function(...) {}
 morethan <- function(...) {}
 interval <- function(...) {}
 
-# TODO: implement filters, check number of samples after filtering the test data and fill expected_samples
 test_cases <- list(
   list(
     method = "by.meta", query = list(Status = include("C")),
-    expected_samples = 0
+    expected_samples = 6
   ),
   list(
-    method = "by.meta", method = "by.meta", query = list(Lane = exclude("A")),
-    expected_samples = 0
+    method = "by.meta", query = list(Lane = exclude("A")),
+    expected_samples = 8
   ),
   list(
     method = "by.meta", query = list(Age = lessthan(23)),
-    expected_samples = 0
+    expected_samples = 10
   ),
   list(
     method = "by.meta", query = list(Age = interval(15, 23)),
-    expected_samples = 0
+    expected_samples = 4
   ),
   list(
     method = "by.meta", query = list(Lane = include("B", "C")),
-    expected_samples = 0
+    expected_samples = 8
   ),
   list(
     method = "by.meta", query = list(Lane = exclude("A", "C")),
-    expected_samples = 0
+    expected_samples = 2
   ),
   list(
     method = "by.meta", query = list(Lane = include("A", "B"), Age = morethan(15)),
-    expected_samples = 0
+    expected_samples = 3
   ),
   list(
-    method = "by.repertoire", query = list(n_clonotypes = morethan(1000)),
-    expected_samples = 0
+    method = "by.repertoire", query = list(n_clonotypes = morethan(6000)),
+    expected_samples = 8
   ),
   list(
     method = "by.repertoire", query = list(n_clones = lessthan(100)),
     expected_samples = 0
   ),
   list(
-    method = "by.rep", query = list(n_clonotypes = morethan(1000)),
-    expected_samples = 0
+    method = "by.rep", query = list(n_clonotypes = morethan(6000)),
+    expected_samples = 8
   ),
   list(
     method = "by.rep", query = list(n_clones = lessthan(100)),
@@ -53,23 +52,23 @@ test_cases <- list(
   ),
   list(
     method = "by.clonotype", query = list(CDR3.aa = exclude("partial", "out_of_frame")),
-    expected_samples = 0
+    expected_samples = 12
   ),
   list(
-    method = "by.clonotype", query = list(Clones = interval(5, 50)),
-    expected_samples = 0
+    method = "by.clonotype", query = list(Clones = interval(150, 200)),
+    expected_samples = 4
   ),
   list(
     method = "by.cl", query = list(CDR3.aa = exclude("partial", "out_of_frame")),
-    expected_samples = 0
+    expected_samples = 12
   ),
   list(
-    method = "by.cl", query = list(Clones = interval(5, 50)),
-    expected_samples = 0
+    method = "by.cl", query = list(Clones = interval(150, 200)),
+    expected_samples = 4
   ),
   list(
     method = "by.gene", query = list(V = exclude("TRBV1", "TRGV11")),
-    expected_samples = 0
+    expected_samples = 12
   ),
   list(
     method = "by.gene", query = list(V = include("TRAV9"), J = include("TRAJ11")),
@@ -78,6 +77,7 @@ test_cases <- list(
 )
 
 for (i in seq_along(test_cases)) {
+  # Arrange
   method <- test_cases[[i]][["method"]]
   query <- test_cases[[i]][["query"]]
   expected_samples <- test_cases[[i]][["expected_samples"]]
@@ -86,12 +86,14 @@ for (i in seq_along(test_cases)) {
   # TODO: implement all filters and remove skip()
   skip("Not implemented")
 
+  # Act
   compute_res <- apply_DF_DT(frame_with_meta, table_with_meta,
     repFilter,
     .method = method,
     .query = query
   )
 
+  # Assert
   test_that(ap(test_name, "compute_"), {
     expect_equal(compute_res[[1]]$data %>% length(), expected_samples)
     expect_equal(compute_res[[1]]$meta %>% nrow(), expected_samples)
