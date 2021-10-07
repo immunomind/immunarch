0767d44c176c9e8df95b1211bf912fc1cdc43120
Aleksandr Popov <fire_2005@mail.ru>
2021-10-07 13:47:14 +0300

    automatic changes after full rebuild

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // fill_vec
 NumericVector fill_vec(NumericVector read_vec, NumericVector read_indices);
 RcppExport SEXP _immunarch_fill_vec(SEXP read_vecSEXP, SEXP read_indicesSEXP) {
