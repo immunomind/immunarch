a475912ca7b2f17e15f41d0f7ef326bc1a4cbe19
Aleksandr Popov <alexander230r@gmail.com>
2022-11-21 23:27:02 +0100

    Tests for repAlignLineage; small fixes

--- tests/testthat/test-align-lineage.R
+++ tests/testthat/test-align-lineage.R
@@ -1,11 +1,17 @@
 data(bcrdata)
 test_bcr_data <- bcrdata$data %>% top(1000)
+test_input <- test_bcr_data %>%
+  seqCluster(seqDist(test_bcr_data), .fixed_threshold = 3) %>%
+  repGermline(.threads = 1) %>%
+  suppressWarnings()
 
 positive_test_cases <- list(
   "Not empty result" = list(
     args = list(
-      .data = test_bcr_data,
-      .threads = 1
+      .data = test_input,
+      .min_lineage_sequences = 2,
+      .prepare_threads = 1,
+      .align_threads = 1
     ),
     assert_function = function(result) {
       expect_equal(immunarch:::has_no_data(result), FALSE)
@@ -14,7 +20,8 @@ positive_test_cases <- list(
   ),
   "Multiple threads" = list(
     args = list(
-      .data = test_bcr_data
+      .data = test_input,
+      .min_lineage_sequences = 2
     ),
     assert_function = function(result) {
       expect_equal(immunarch:::has_no_data(result), FALSE)
@@ -23,8 +30,10 @@ positive_test_cases <- list(
   ),
   "Dataframe only" = list(
     args = list(
-      .data = test_bcr_data[["full_clones"]],
-      .threads = 1
+      .data = test_input[["full_clones"]],
+      .min_lineage_sequences = 2,
+      .prepare_threads = 1,
+      .align_threads = 1
     ),
     assert_function = function(result) {
       expect_equal(immunarch:::has_no_data(result), FALSE)
@@ -40,7 +49,7 @@ for (i in seq_along(positive_test_cases)) {
   assert_function <- positive_test_cases[[i]][["assert_function"]]
 
   # Act
-  result <- suppressWarnings(do.call(repGermline, args))
+  result <- suppressWarnings(do.call(repAlignLineage, args))
 
   # Assert
   test_that(
@@ -55,10 +64,25 @@ negative_test_cases <- list(
       .data = bcrdata
     )
   ),
-  "Missing column" = list(
+  "Missing columns" = list(
     args = list(
-      .data = subset(test_bcr_data[["full_clones"]], select = -c(get("FR1.nt"))),
-      .threads = 1
+      .data = test_bcr_data[["full_clones"]],
+      .prepare_threads = 1,
+      .align_threads = 1
+    )
+  ),
+  "Missing Cluster column" = list(
+    args = list(
+      .data = subset(test_input[["full_clones"]], select = -c(get("Cluster"))),
+      .prepare_threads = 1,
+      .align_threads = 1
+    )
+  ),
+  "Missing Germline.sequence column" = list(
+    args = list(
+      .data = subset(test_input[["full_clones"]], select = -c(get("Germline.sequence"))),
+      .prepare_threads = 1,
+      .align_threads = 1
     )
   )
 )
@@ -71,6 +95,6 @@ for (i in seq_along(negative_test_cases)) {
   # Act, Assert
   test_that(
     test_name,
-    expect_error(suppressWarnings(do.call(repGermline, args)))
+    expect_error(suppressWarnings(do.call(repAlignLineage, args)))
   )
 }
