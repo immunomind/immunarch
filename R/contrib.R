17393a5a8718f5194022b564fe2ef65c3f55a3cf
Vadim Nazarov <2979713+vadimnazarov@users.noreply.github.com>
2023-08-21 15:49:00 +0200

    Add the contrib.R file for easier management of external contributions.

---
+++ R/contrib.R
@@ -0,0 +1,11 @@
+#
+# This is a file with external contributions to Immunarch. Contributions can either have
+# their own licensing terms, or follow the general Immunarch's contributions. The authors and license
+# will be specified separately in each contribution.
+#
+# In order to add a new contribution, fork the Immunarch's repository, change this file ("contrib.R")
+# and open a pull request to the main repository.
+#
+# Good luck and thank you for contributing to Immunarch! We will list all notable contributors on the website
+# and in the repository.
+#
