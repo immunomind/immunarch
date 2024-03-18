93b6e81e16249066083afca45ccaf085dc356707
Vadim Nazarov <2979713+vadimnazarov@users.noreply.github.com>
2024-03-18 22:13:25 +0200

    wip: add a draft file with functions for the future version of immunarch with better naming for functions and immundata support

---
+++ R/immunarch-remaster.R
@@ -0,0 +1,25 @@
+# .check_immundata <- function (.object) {
+#   if (!is.instance(.object, "ImmunData")) {
+#     stop("Error: the input object is not of class ImmunData. Immunarch works on ImmunData only. Helpful manual: ...")
+#   }
+# }
+#
+# .repertoire_overlap <- function (.data, .method, .verbose = TRUE, ...) {
+#   check_immundata(.data)
+# }
+#
+# .gene_usage <- function (.data, .gene, .type, .use_counts, .norm, .gene_vec) {
+#
+# }
+#
+# .repertoire_diversity <- function (.data, .method, .verbose = TRUE, ...) {
+#
+# }
+#
+# .track_clonotypes <- function () {
+#
+# }
+#
+# .public_repertoire <- function () {
+#
+# }
